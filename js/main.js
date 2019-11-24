0911a7c3bca8d3c7879eaaefe7e5eeb487c115c5
Emma Ling <emmaling27@gmail.com>
2019-11-24 17:25:33 -0500

    json w data and map functional

--- js/main.js
+++ js/main.js
@@ -1,6 +1,18 @@
 /* main JS file */
-d3.csv("data/cleaned_outcomes.csv", data => {
-    console.log(data);
+// Taken from https://dev.to/afewminutesofcode/how-to-convert-an-array-into-an-object-in-javascript-25a4
+const convertArrayToObject = (array, key) => {
+    const initialValue = {};
+    return array.reduce((obj, item) => {
+        return {
+            ...obj,
+            [item[key]]: item,
+        };
+    }, initialValue);
+};
+
+d3.json("data/kir.json", data => {
+    // data = convertArrayToObject(data, 'tract');
+    // console.log("new data:", data);
     const map = new MapVis(data);
     // map.initVis();
 });
--- js/map.js
+++ js/map.js
@@ -11,24 +11,38 @@ class MapVis {
             .attr("height", this.height);
         this.path = d3.geoPath()
             .projection(null);
+        const lowColor = "#4997B3";
+        const highColor = "#B37029";
+        this.colorScale = d3.scaleLinear()
+            .domain([0,1])
+            .range([lowColor, highColor])
+            .interpolate(d3.interpolateLab);
         this.initVis();
     }
 
     initVis() {
         const vis = this;
+        // console.log(vis.data);
+
         d3.json("data/census_tract_topo.json", function(error, us) {
             if (error) throw error;
-
             const tracts = us.objects.tracts;
-
-            vis.svg.append("g")
+            const map = vis.svg.append("g")
                 .selectAll("path")
-                .data(topojson.feature(us, tracts).features)
-                .enter().append("path")
+                .data(topojson.feature(us, tracts).features);
+            map.enter().append("path")
                 .attr("class", "tract")
                 .attr("d", vis.path)
-                .append("title")
-                .text(function(d, i) { return d.id; });
+                .attr("fill", d => {
+                    // console.log(vis.data[parseInt(d.properties.TRACTCE)]);
+                    if (vis.data[parseInt(d.properties.TRACTCE)]) {
+                        return vis.colorScale(vis.data[parseInt(d.properties.TRACTCE)]["kir_white_pooled_p100"])
+                    } else {
+                        return "gray";
+                    }
+                })
+                // .append("title")
+                // .text(function(d, i) { return d.id; });
 
             // vis.svg.append("path")
             //     .attr("class", "tract-border")
