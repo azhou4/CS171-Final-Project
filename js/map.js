6892d93c9fc330d93a4bab4b6a48b720aa9ca129
Emma Ling <emmaling27@gmail.com>
2019-11-24 17:39:59 -0500

    legend

--- js/map.js
+++ js/map.js
@@ -17,6 +17,17 @@ class MapVis {
             .domain([0,1])
             .range([lowColor, highColor])
             .interpolate(d3.interpolateLab);
+        this.legendWidth = 200;
+        const divisions = 100;
+        this.sectionWidth = Math.floor(this.legendWidth / divisions);
+        this.legendData = [];
+        for (let i = 0; i < this.legendWidth; i += this.sectionWidth) {
+            this.legendData.push(i);
+        }
+        this.legendColorScale = d3.scaleLinear()
+            .domain([0, this.legendData.length - 1])
+            .range([lowColor, highColor])
+            .interpolate(d3.interpolateLab);
         this.initVis();
     }
 
@@ -40,7 +51,7 @@ class MapVis {
                     } else {
                         return "gray";
                     }
-                })
+                });
                 // .append("title")
                 // .text(function(d, i) { return d.id; });
 
@@ -48,6 +59,26 @@ class MapVis {
             //     .attr("class", "tract-border")
             //     .datum(topojson.mesh(us, tracts, function(a, b) { return a !== b; }))
             //     .attr("d", vis.path);
+            const legend = vis.svg.append("g")
+                .attr("class", "legend")
+                .attr("transform", "translate(300, 0)");
+            legend.selectAll('rect')
+                .data(vis.legendData)
+                .enter()
+                .append('rect')
+                .attr("x", function(d) { return d; })
+                .attr("y", 10)
+                .attr("height", 10)
+                .attr("width", vis.sectionWidth)
+                .attr('fill', function(d, i) { return vis.legendColorScale(i)});
+            legend.append("text").text(() => "0%")
+                .attr("transform","translate(0,30)")
+                .style("fill", "black")
+                .style("font-size", "10px");
+            legend.append("text").text(() => "100%")
+                .attr("transform","translate("+(vis.legendWidth-20)+",30)")
+                .style("font-size", "10px");
+        
         });
     }
 }
